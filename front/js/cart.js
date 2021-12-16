2ac794c5b64d5835011e707feffdf91469cb793e
TCGit08 <tcalvetp@laposte.net>
2021-12-16 11:05:09 +0100

    Page Panier js - correction inser html

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,16 +10,19 @@
 
 // Changement titre page panier (Cart -> Kanap - Panier)
 
-let titrePagePanier = document.querySelector('title');       // Pointage de la balise title
-titrePagePanier.innerHTML = "Kanap - Panier";                // Changement dynamique du nom
+let titrePagePanier = document.querySelector('title');          // Pointage de la balise title
+titrePagePanier.innerHTML = "Kanap - Panier";                   // Changement dynamique du nom
 
 
 
 //  Récupération des éléments du panier
 
 let PanierProduit = JSON.parse(localStorage.getItem('Selection'));
+
 console.log(PanierProduit);
+
 console.log(typeof(PanierProduit));
+
 
 
 // Traitement des cas possibles de PanierProduit et remplissage de <section id="cart__items">.
@@ -28,45 +31,55 @@ console.log(typeof(PanierProduit));
 
 
 if (PanierProduit === null || PanierProduit == 0) {
-    let InserPanierVide = document.getElementById('#cart__items');
+    
+    let InserPanierVide = document.getElementById('cart__items');
+    
+    console.log(InserPanierVide);
+
     InserPanierVide.insertAdjacentHTML('afterend',
-        '<div class="cart__item__img">', 
-                '<p> Votre panier ne comporte actuellement aucun produit </p>',
-        '</div>'
+
+        `<div class="cart__item__img"> 
+                <p> Votre panier ne comporte actuellement aucun produit </p>
+        </div>`
+
     );
+        
         
 }else{
 
     // Cas où le panier n'est pas vide
 
-    for(i=0; i< PanierProduit; i++){
+    for(i=0; i< PanierProduit.lengh; i++){
 
-    // Utiliser la section en commentaire dans cart.html et remplir avec les éléments des produits sélectionnés (cf product.js)
+        // Utiliser la section en commentaire dans cart.html et remplir avec les éléments des produits sélectionnés (cf product.js)
 
-        let InserPanierNONVide = document.getElementById('#cart__items');
-        InserPanierNONVide.insertAdjacentHTML('afterend',
+        let InserPanierNONVide = document.getElementById('cart__items');
+        
+        console.log(InserPanierNONVide);
+
+        InserPanierNONVide.insertAdjacentHTML('afterend',  
             
-            '<article class="cart__item" data-id="${PanierProduit[i].IdSelectionProduit}" data-color="${PanierProduit[i].CouleurSelectionProduit}">',
-                '<div class="cart__item__img">',
-                  '<img src="${PanierProduit[i].ImgSelectionProduit}" alt="${PanierProduit[i].AltTxtSelectionProduit}"/>',
-                '</div>', 
-                '<div class="cart__item__content">', 
-                    '<div class="cart__item__content__description">', 
-                        '<h2>${PanierProduit[i].NomSelectionProduit}</h2>', 
-                        '<p>${PanierProduit[i].CouleurSelectionProduit}</p>', 
-                        '<p>${PanierProduit[i].PrixSelectionProduit}</p>', 
-                    '</div>', 
-                    '<div class="cart__item__content__settings">', 
-                        '<div class="cart__item__content__settings__quantity">', 
-                            '<p>Qté : </p>', 
-                            '<input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${PanierProduit[i].QuantiteSelectionProduit}"/>', 
-                        '</div>', 
-                        '<div class="cart__item__content__settings__delete">', 
-                            '<p class="deleteItem">Supprimer</p>', 
-                        '</div>', 
-                    '</div>', 
-                '</div>', 
-            '</article>'
+            `<article class="cart__item" data-id="${PanierProduit[i].IdSelectionProduit}" data-color="${PanierProduit[i].CouleurSelectionProduit}">
+                <div class="cart__item__img">
+                  <img src="${PanierProduit[i].ImgSelectionProduit}" alt="${PanierProduit[i].AltTxtSelectionProduit}"/>
+                </div> 
+                <div class="cart__item__content"> 
+                    <div class="cart__item__content__description">
+                        <h2>${PanierProduit[i].NomSelectionProduit}</h2>
+                        <p>${PanierProduit[i].CouleurSelectionProduit}</p>
+                        <p>${PanierProduit[i].PrixSelectionProduit}</p>
+                    </div> 
+                    <div class="cart__item__content__settings">
+                        <div class="cart__item__content__settings__quantity">
+                            <p>Qté : </p>
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${PanierProduit[i].QuantiteSelectionProduit}"/> 
+                        </div>
+                        <div class="cart__item__content__settings__delete">
+                            <p class="deleteItem">Supprimer</p>
+                        </div>
+                    </div>
+                </div> 
+            </article>`
 
         );            
 
